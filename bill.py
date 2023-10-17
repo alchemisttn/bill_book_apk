25021a0d4d210d8a8b27bb7e8aa384634aaeb827
alchemist <tnbipin@gmail.com>
2023-10-17 11:38:45 +0530

    2

--- bill.py
+++ bill.py
@@ -1,5 +1,5 @@
 from kivy.uix.screenmanager import Screen
 
 
-class HistoryIndex(Screen):
+class Bill(Screen):
     pass
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 
 from add_product import AddProduct
+from bill import Bill
+from history import History
+from history_index import HistoryIndex
 from index import Index
 from login import Login
 
@@ -10,12 +13,18 @@ from login import Login
 class ScreenHandler(ScreenManager):
     def __init__(self):
         super().__init__()
-        self.add_widget(Login(name='login_page'))
-        self.add_widget(Index(name='index_page'))
-        self.add_widget(AddProduct(name='add_product'))
+        # self.add_widget(Login(name='login_page'))
+        # self.add_widget(Index(name='index_page'))
+        # self.add_widget(AddProduct(name='add_product_page'))
+        # self.add_widget(HistoryIndex(name='history_index_page'))
+        # self.add_widget(History(name='history_page'))
+        self.add_widget(Bill(name='bill_page'))
         # self.current = 'login_page'
         # self.current = 'index_page'
-        self.current = 'add_product'
+        # self.current = 'add_product_page'
+        # self.current = 'history_index_page'
+        # self.current = 'history_page'
+        self.current = 'bill_page'
 
 
 class MyApp_Entrance(MDApp):
@@ -24,6 +33,9 @@ class MyApp_Entrance(MDApp):
         Builder.load_file('login.kv')  # todo check if user is already logged in and load
         Builder.load_file('index.kv')
         Builder.load_file('add_product.kv')
+        Builder.load_file('history_index.kv')
+        Builder.load_file('history.kv')
+        Builder.load_file('bill.kv')
         return ScreenHandler()
 
 
